0b653f28cc78d19aee0eda10dd9d536408febe71
Ronny Acacio <ronnyacacio27@gmail.com>
2020-06-22 20:57:11 -0300

    feat: song game boy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'react-native';
+import { Audio } from 'expo-av';
 
 import Home from './screens/Home';
-// import Player from './components/Player';
 
 export default function App() {
+  const [active, setActive] = useState(false);
+
+  const soundObject = new Audio.Sound();
+
+  async function playSong() {
+    await soundObject.loadAsync(require('../music/pokesong.mp3'));
+    await soundObject.playAsync();
+    await soundObject.setIsLoopingAsync(true);
+  }
+
+  useEffect(() => {
+    if (!active) playSong();
+    setActive(true);
+  }, []);
+
   return (
     <>
       <StatusBar backgroundColor="transparent" translucent />
-
       <Home />
     </>
   );
